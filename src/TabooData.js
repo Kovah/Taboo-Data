5469080f770445b203b32598f4c94b3aeb229187
Kovah <mail@kovah.de>
2019-05-21 14:33:02 +0200

    Add missing categories to JS helper

--- src/TabooData.js
+++ src/TabooData.js
@@ -2,6 +2,13 @@ import { categories } from './data/categories';
 import { animals } from './data/de/animals';
 import { cars } from './data/de/cars';
 import { cityCountry } from './data/de/city-country';
+import { food } from './data/de/food';
+import { literature } from './data/de/literature';
+import { people } from './data/de/people';
+import { sports } from './data/de/sports';
+import { things } from './data/de/things';
+import { tv } from './data/de/tv';
+import { web } from './data/de/web';
 
 export default class TabooData {
 
@@ -10,6 +17,13 @@ export default class TabooData {
       'animals': animals,
       'cars': cars,
       'cityCountry': cityCountry,
+      'food': food,
+      'literature': literature,
+      'people': people,
+      'sports': sports,
+      'things': things,
+      'tv': tv,
+      'web': web,
     };
   };
 
